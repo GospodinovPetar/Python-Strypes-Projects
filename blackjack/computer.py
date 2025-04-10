6a784c207a73ebfbe2a2b22fe9887fced526f911
gospodinovpetar <petarjordanov2003@gmail.com>
2025-04-10 18:43:14 +0300

    Deleting old reposotories

--- blackjack/computer.py
+++
@@ -1,6 +0,0 @@
-class Computer:
-    def __init__(self, points : int = 0):
-        #Computers points
-        self.points : int = points
-        #Computers hand
-        self.hand : list = []
--- blackjack/player.py
+++
@@ -1,8 +0,0 @@
-class Player:
-    def __init__(self, total_amount : int, points : int = 0):
-        #Players points
-        self.points : int = points
-        #Players total amount of money
-        self.total_amount : int = total_amount
-        #Players hand
-        self.hand : list = []
