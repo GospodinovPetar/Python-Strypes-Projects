517e8843e66a06cd21f4f73ac791cf2a22f38af6
gospodinovpetar <petarjordanov2003@gmail.com>
2025-04-10 20:06:06 +0300

    Fallback logic if we dont find links or cant access page

--- Complete Python Bootcamp/page_scraper/main.py
+++ Complete Python Bootcamp/page_scraper/main.py
@@ -7,18 +7,23 @@ def link_finder(beautiful_soup):
     """"
     This function finds all the links in the given webpage.
     """
-    links = []
+    links : list= []
 
     for link in beautiful_soup.find_all('a', href=True):  # href=True ensures that we only get valid links
         l = link['href']
         if l.startswith('http') or l.startswith('https'):
             links.append(l)
 
-    return f"Links found:' '\n' {'\n'.join(links)}"
+
+    if len(links) == 0:
+        return 'No links found or webpage is inaccessible.'
+    else:
+        return f"Links found:' '\n' {'\n'.join(links)}"
 
 # Send a GET request to the URL
+# try:
 response = requests.get(input('Enter URL: '))
-response.raise_for_status()  # Raise an exception for 4xx/5xx responses
+# except Exception as e:
 
 # Parse the content using BeautifulSoup
 soup = BeautifulSoup(response.text, 'html.parser')
