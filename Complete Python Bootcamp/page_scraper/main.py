2fae7f1e0ea1258740d26913e5f72022345b99d6
Петър Господинов <53982142+GospodinovPetar@users.noreply.github.com>
2025-04-10 20:02:42 +0300

    Update main.py

--- Complete Python Bootcamp/page_scraper/main.py
+++ Complete Python Bootcamp/page_scraper/main.py
@@ -7,7 +7,7 @@ def link_finder(beautiful_soup):
     """"
     This function finds all the links in the given webpage.
     """
-    links = []
+    links : list = []
 
     for link in beautiful_soup.find_all('a', href=True):  # href=True ensures that we only get valid links
         l = link['href']
@@ -18,7 +18,6 @@ def link_finder(beautiful_soup):
 
 # Send a GET request to the URL
 response = requests.get(input('Enter URL: '))
-response.raise_for_status()  # Raise an exception for 4xx/5xx responses
 
 # Parse the content using BeautifulSoup
 soup = BeautifulSoup(response.text, 'html.parser')
