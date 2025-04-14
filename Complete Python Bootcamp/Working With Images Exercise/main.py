4a2b286713f090c3a2a0f4be0c4353224d124afc
gospodinovpetar <petarjordanov2003@gmail.com>
2025-04-14 17:15:52 +0300

    Creating the exercise

---
+++ Complete Python Bootcamp/Working With Images Exercise/main.py
@@ -0,0 +1,10 @@
+from PIL import Image
+
+words = Image.open("words.png")
+word_windows = Image.open("word_windows.png")
+mask = word_windows.resize((1015, 559))
+
+mask.putalpha(200)
+words.paste(mask, (0,0), mask)
+
+words.show()
