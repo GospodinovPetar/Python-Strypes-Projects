7ed9d62f847084395ed27ce81d85068c71e1f950
gospodinovpetar <petarjordanov2003@gmail.com>
2025-04-22 15:18:59 +0300

    .

--- complete_python_bootcamp/projects/blackjack/test_blackjack.py
+++ complete_python_bootcamp/projects/blackjack/test_blackjack.py
@@ -2,6 +2,7 @@ from unittest.mock import patch
 import pytest
 from computer import Computer
 from player import Player
+from main import handle_game_round
 from main import (
     count_points,
     reset_points,
