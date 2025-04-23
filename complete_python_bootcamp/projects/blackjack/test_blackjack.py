0d063a07a4cc8e75f42a49d60d5e17e75cc31846
Петър Господинов <53982142+GospodinovPetar@users.noreply.github.com>
2025-04-23 12:26:18 +0300

    Docstring

--- complete_python_bootcamp/projects/blackjack/test_blackjack.py
+++ complete_python_bootcamp/projects/blackjack/test_blackjack.py
@@ -380,6 +380,13 @@ def test_game_busts_after_hit(mock_input: patch):
 
 
 def test_new_game():
+  """
+    Tests the initialization of a new game.
+
+    This test verifies that the function should return "New Game!" when the old game has finished and a new one is initialized, that happens 
+    when the given parameter is different than "Game continues..".
+    """
+
     player = Player(100)
     computer = Computer()
 
